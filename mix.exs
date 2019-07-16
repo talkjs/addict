8e58da07e1d842ab2c6d316a9d048c57976402e1
Marcin Koba <mrcnkoba@gmail.com>
2019-07-16 20:59:43 +0200

    Bump ecto

--- mix.exs
+++ mix.exs
@@ -26,7 +26,7 @@ defmodule Addict.Mixfile do
   defp deps do
     [{:cowboy, "~> 2.0"},
      {:phoenix, "~> 1.4"},
-     {:ecto, "~> 2.0"},
+     {:ecto, "~> 3.0"},
      {:comeonin, "~> 2.1" },
      {:mailgun, "~> 0.1"},
      {:mock, "~> 0.1.3", only: :test},
